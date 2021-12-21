974cc6eb6705ee776a1a53b729327eb512fc372d
Prabhudarshan Singh <Prabhu25.1975@gmail.com>
2021-12-21 18:59:39 +0530

    Fetching Movies with a Traditional Promise - Testing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-export function fetchMovies(){
+const movies = require("./data/movies.json");
+const {fetchWithTimeout}=require("./services");
 
+
+
+export function fetchMovies(){
+    const resolveFunction = () => movies;
+
+    return fetchWithTimeout(1000).then(resolveFunction);
 }
+
+let moviePromise=fetchMovies();
+
+moviePromise.then((results)=>console.log(results));
 
